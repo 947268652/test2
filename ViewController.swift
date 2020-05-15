a0b65d0f7ffb3cf661ab2fd73c55f35f6a9dfc5d
chengzutie <chengzutie@lizhi.fm>
2020-05-15 14:22:20 +0800

    提交人：iRon 提交内容：ViewController

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by chengzutie on 2020/5/14.
+//  Copyright © 2020 chengzutie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .yellow
+    }
+
+
+}
+
